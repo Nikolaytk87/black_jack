8a89926455a5b4cef51a6935b16268b76cd1352c
Nikolay <nikolaytk87@mail.ru>
2022-10-25 11:23:38 -0500

    Add scores

--- player.rb
+++ player.rb
@@ -1,13 +1,15 @@
 class Player
-  attr_reader :name, :bank, :current_cards
+  attr_reader :name, :bank, :current_cards, :score, :rate
 
   def initialize(name)
     @name = name
     @bank = 100
+    @score = 0
+    @rate = 10
     @current_cards ||= []
   end
 
   protected
 
-  attr_writer :bank
+  attr_writer :bank, :score
 end
