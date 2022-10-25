0b446084ef865a69e81e3041e5ef8ea3d5e2ff11
Nikolay <nikolaytk87@mail.ru>
2022-10-25 11:16:22 -0500

    Add current_cards

--- diller.rb
+++ diller.rb
@@ -1,9 +1,10 @@
 class Diller
-  attr_reader :name, :bank
+  attr_reader :name, :bank, :current_cards
 
   def initialize(name: 'Diller')
     @name = name
     @bank = 100
+    @current_cards ||= []
   end
 
   protected
--- player.rb
+++ player.rb
@@ -1,9 +1,10 @@
 class Player
-  attr_reader :name, :bank
+  attr_reader :name, :bank, :current_cards
 
   def initialize(name)
     @name = name
     @bank = 100
+    @current_cards ||= []
   end
 
   protected
