0cfef5780cd96f1db838f355d49ede28b6c18e1a
Nikolay <nikolaytk87@mail.ru>
2022-10-28 15:44:19 -0500

    refactoring game

--- game.rb
+++ game.rb
@@ -2,6 +2,7 @@
 
 require_relative 'inputs'
 class Game
+  MAX_POINTS = 21
   include Inputs
   attr_reader :player, :dealer
   attr_accessor :deck, :bank
@@ -54,14 +55,9 @@ class Game
   end
 
   def player_add_cards
-    if player.cards.size == 2
-      card = player.cards.concat(give_cards).last
-      puts "Add card #{card.name} #{card.suit}. All Points: #{player.score}"
-      maximum_cards? ? open_cards : dealer_move
-    else
-      puts 'Too much cards'
-      maximum_cards? ? open_cards : player_move(input_player_choice)
-    end
+    card = player.cards.concat(give_cards).last
+    puts "Add card #{card.name} #{card.suit}. All Points: #{player.score}"
+    maximum_cards? ? open_cards : dealer_move
   end
 
   def dealer_move
@@ -86,24 +82,36 @@ class Game
   end
 
   def calculating
-    diff_player = 21 - player.score
-    diff_dealer = 21 - dealer.score
+    diff_player = MAX_POINTS - player.score
+    diff_dealer = MAX_POINTS - dealer.score
     if diff_player.negative? || (diff_player > diff_dealer && !diff_dealer.negative?)
-      dealer.bank += bank
-      puts "Alas,#{player.name} you lost"
+      action_player_lost
     elsif diff_player < diff_dealer || diff_dealer.negative?
-      player.bank += bank
-      puts "Congratulations, #{player.name} you won"
+      action_player_win
     elsif player.score == dealer.score
-      puts 'Tie. Number of points equal'
-      self.bank /= 2
-      player.bank += bank
-      dealer.bank += bank
+      action_tie
     end
     self.bank = 0
   end
 
-  protected
+  def action_player_lost
+    dealer.bank += bank
+    puts "Alas,#{player.name} you lost"
+  end
 
-  attr_accessor :game_bank
+  def action_player_win
+    player.bank += bank
+    puts "Congratulations, #{player.name} you won"
+  end
+
+  def action_tie
+    puts 'Tie. Number of points equal'
+    split_users_bank
+  end
+
+  def split_users_bank
+    self.bank /= 2
+    player.bank += bank
+    dealer.bank += bank
+  end
 end
